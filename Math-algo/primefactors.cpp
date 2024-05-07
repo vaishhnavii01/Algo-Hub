3fab1925ae13181fd0578d3b1e4924d739fd1cbf
vaishhnavii01 <agrawalvaishnavi12@gmail.com>
2024-05-08 01:29:19 +0530

    ADDED

---
+++ Math-algo/primefactors.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        for(int i = 2; i * i <= n; i++) {
+            if(n % i == 0) {
+                int cnt = 0;
+                while(n % i == 0) {
+                    cnt++;
+                    n = n / i;
+                }
+                cout << i << "^" << cnt << " "; 
+            }
+        }
+        if(n > 1) {
+            cout << n << "^" << 1 << " "; 
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
